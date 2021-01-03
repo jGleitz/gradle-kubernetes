2214d0171952df8e197b4f4d1c1bd3004de9b2cc
Joshua Gleitze <dev@joshuagleitze.de>
2021-01-03 14:19:08 +0100

    refactor: replace Path.of with kotlin’s Path

--- common/src/testFixtures/kotlin/GradleIntegrationTestProject.kt
+++ common/src/testFixtures/kotlin/GradleIntegrationTestProject.kt
@@ -2,13 +2,13 @@ package de.joshuagleitze.test
 
 import org.gradle.testkit.runner.BuildResult
 import org.gradle.testkit.runner.GradleRunner
-import java.nio.file.Path
+import kotlin.io.path.Path
 import kotlin.io.path.createDirectories
 import kotlin.io.path.div
 import kotlin.io.path.writeText
 
 object GradleIntegrationTestProject {
-	private val baseDir = Path.of("build").toAbsolutePath() / "test-outputs" / "[${GradleIntegrationTestProject::class.simpleName}]"
+	private val baseDir = Path("build").toAbsolutePath() / "test-outputs" / "[${GradleIntegrationTestProject::class.simpleName}]"
 	val projectDir = baseDir / "testProject"
 	val gradleHome = baseDir / "gradleHome"
 
