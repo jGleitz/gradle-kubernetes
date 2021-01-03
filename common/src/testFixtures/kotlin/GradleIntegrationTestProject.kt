fedb54ea5195aadb1c36275096ccfa0e64ffe5e7
Joshua Gleitze <dev@joshuagleitze.de>
2021-01-03 16:05:42 +0100

    build: enable parallel build

--- common/src/testFixtures/kotlin/GradleIntegrationTestProject.kt
+++ common/src/testFixtures/kotlin/GradleIntegrationTestProject.kt
@@ -7,6 +7,11 @@ import kotlin.io.path.createDirectories
 import kotlin.io.path.div
 import kotlin.io.path.writeText
 
+/**
+ * Offers access to a shared integration test Gradle project. The project is shared for all tests in the same Gradle project. This means
+ * that tests can re-use the test project to improve performance, but also that the tests *cannot* be executed in parallel. However, since
+ * different Gradle projects will use different test projects, tests from different Gradle projects *can* be executed in parallel.
+ */
 object GradleIntegrationTestProject {
 	private val baseDir = Path("build").toAbsolutePath() / "test-outputs" / "[${GradleIntegrationTestProject::class.simpleName}]"
 	val projectDir = baseDir / "testProject"
