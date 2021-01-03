4a50824e10501811c169ef44a2eca87d531e3fca
Joshua Gleitze <dev@joshuagleitze.de>
2021-01-03 18:18:07 +0100

    build: remove spek and add kotest

--- common/build.gradle.kts
+++ common/build.gradle.kts
@@ -6,14 +6,11 @@ plugins {
 }
 
 dependencies {
-	val spekVersion = "2.0.15"
-
 	implementation(name = "string-notation", version = "1.4.0", group = "de.joshuagleitze")
 
 	testFixturesImplementation(gradleKotlinDsl())
 	testFixturesImplementation(gradleApi())
 	testFixturesImplementation(gradleTestKit())
-	testFixturesImplementation(name = "spek-dsl-jvm", version = spekVersion, group = "org.spekframework.spek2")
 	testFixturesImplementation(name = "atrium-fluent-en_GB", version = "0.16.0", group = "ch.tutteli.atrium")
 
 	constraints {
