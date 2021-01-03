b692fead7b989ad7162c10e8daa3af925434256f
Joshua Gleitze <dev@joshuagleitze.de>
2021-01-03 18:18:07 +0100

    build: remove spek and add kotest

--- kubernetes/build.gradle.kts
+++ kubernetes/build.gradle.kts
@@ -15,19 +15,16 @@ gradlePlugin {
 }
 
 dependencies {
-	val spekVersion = "2.0.15"
-
 	implementation(gradleKotlinDsl())
 	implementation(project(":common"))
 	implementation(name = "string-notation", version = "1.4.0", group = "de.joshuagleitze")
 
 	testImplementation(testFixtures(project(":common")))
-	testImplementation(name = "spek-dsl-jvm", version = spekVersion, group = "org.spekframework.spek2")
-	testImplementation(name = "atrium-fluent-en_GB", version = "0.16.0", group = "ch.tutteli.atrium")
+	testImplementation(name = "kotest-runner-junit5", version = "4.3.2", group = "io.kotest")
+	testImplementation(name = "atrium-fluent-en_GB", version = "0.15.0", group = "ch.tutteli.atrium")
 	testImplementation(name = "atrium-gradle-testkit-fluent-en", version = "1.0.1", group = "de.joshuagleitze")
-	testImplementation(name = "spek-testfiles", version = "2.0.0", group = "de.joshuagleitze")
-	testImplementation(name = "mockk", version = "1.11.0", group = "io.mockk")
-	testRuntimeOnly(name = "spek-runner-junit5", version = spekVersion, group = "org.spekframework.spek2")
+	testImplementation(name = "kotest-files", version = "2.0.0", group = "de.joshuagleitze")
+	testImplementation(name = "mockk", version = "1.10.4", group = "io.mockk")
 
     constraints {
         testImplementation(kotlin("reflect", version = KotlinCompilerVersion.VERSION))
